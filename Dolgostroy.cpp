28fb23cc4acdb86d0a217cb03b7dfac1425a7164
uglubakon <gmodik1231@gmail.com>
2021-12-13 16:45:53 +0300

    2021-12-13_0445 PM

--- Dolgostroy.cpp
+++ Dolgostroy.cpp
@@ -6,7 +6,7 @@
 std::string PointsText = "Points: ";
 
 //BUTTON PLACEMENT
-int PointsButtonPosX = 150;
+int PointsButtonPosX = 100;
 int PointsButtonPosY = 200;
 int PointsButtonWidth = 100;
 int PointsButtonHeight = 50;
@@ -19,10 +19,33 @@ int FrameCounterOneSecondProducerOne = 0;
 int Points = 0;
 int AmountOfProducersOne = 0;
 int CostOfProducerOne = 30;
+int CostOfProducerTwo = 120;
+
+//PRODUCER EXECS
 
 void ProducerOne()
 {
     Points = Points + 1 * AmountOfProducersOne;
+}
+
+void ProducerTwo()
+{
+    Points = Points + 5 * AmountOfProducersOne;
+}
+
+void ProducerThree()
+{
+    Points = Points + 20 * AmountOfProducersOne;
+}
+
+void ProducerFour()
+{
+    Points = Points + 50 * AmountOfProducersOne;
+}
+
+void ProducerFive()
+{
+    Points = Points + 100 * AmountOfProducersOne;
 }
 
 void FrameCounterOneSecondProducerOneFunc() 
@@ -54,20 +77,38 @@ int main(void)
 
     while (!WindowShouldClose())
     {
+        //DRAWING 
         BeginDrawing();
         ClearBackground(WHITE);
+
+        //MOUSE POSITION CALL
         int MouseX = GetMouseX();
         int MouseY = GetMouseY();
+
+        //BUTTONS CALLS
+        DrawRectangle(100, 100, 133, 30, BLACK);
+        DrawRectangle(100, 166, 133, 30, BLACK);
+        DrawRectangle(100, 233, 133, 30, BLACK);
+        DrawRectangle(100, 300, 133, 30, BLACK);
+        DrawRectangle(100, 366, 133, 30, BLACK);
+
+        //TEXT CALLS
+        DrawText("Producer I", 103, 105, 20, WHITE);
+        DrawText("Producer II", 103, 171, 20, WHITE);
+        DrawText("Producer III", 103, 238, 20, WHITE);
+        DrawText("Producer IV", 103, 305, 20, WHITE);
+        DrawText("Producer V", 103, 371, 20, WHITE);
+
+        //COUNTER CALLS
         FrameCounterOneSecondPointsFunc();
         FrameCounterOneSecondProducerOneFunc();
 
-        DrawRectangle(150, 200, 100, 50, BLACK);
         std::string PointsToText = std::to_string(Points);
         std::string FormatedString = PointsText + PointsToText;
-        DrawText(FormatedString.c_str(), 100, 100, 20, BLACK);
-        DrawText("Producer I", 200, 200, 20, BLACK);
+        DrawText(FormatedString.c_str(), 110, 50, 20, BLACK);
         if(IsMouseButtonReleased(0) && MouseX > PointsButtonPosX && MouseY > PointsButtonPosY 
-                                    && MouseX < PointsButtonPosX + PointsButtonWidth && MouseY < PointsButtonPosY + PointsButtonHeight
+                                    && MouseX < PointsButtonPosX + PointsButtonWidth 
+                                    && MouseY < PointsButtonPosY + PointsButtonHeight
                                     && Points >= CostOfProducerOne)
         { 
             AmountOfProducersOne++;
